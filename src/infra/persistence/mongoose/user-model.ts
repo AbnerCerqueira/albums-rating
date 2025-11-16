0452663a512b1075ed5a2ad35ebb3a1e6fc318dc
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-11-15 22:28:02 -0300

    :bug: fix(user): update model

--- src/infra/persistence/mongoose/user-model.ts
+++ src/infra/persistence/mongoose/user-model.ts
@@ -1,37 +1,34 @@
-import { model, Schema, type SchemaTypeOptions } from 'mongoose'
+import { model, Schema } from 'mongoose'
 
 export type UserData = {
   username: string
   password: string
 }
 
-type UserSchema = {
-  [K in keyof UserData]: SchemaTypeOptions<UserData>[K]
-}
-
-const userSchema: UserSchema = {
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-}
-
-const user = new Schema<UserSchema>(userSchema, {
-  timestamps: true,
-  versionKey: false,
-  toObject: {
-    // biome-ignore lint/suspicious/noExplicitAny: faz sentido
-    transform(_, ret: any) {
-      const obj = ret
-      obj.id = obj._id.toString()
-      return obj
+const userSchema = new Schema<UserData>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
     },
   },
-})
+  {
+    timestamps: true,
+    versionKey: false,
+    toObject: {
+      // biome-ignore lint/suspicious/noExplicitAny: faz sentido
+      transform(_, ret: any) {
+        const obj = ret
+        obj.id = obj._id.toString()
+        return obj
+      },
+    },
+  }
+)
 
-export const UserModel = model('users', user)
+export const UserModel = model('users', userSchema)
