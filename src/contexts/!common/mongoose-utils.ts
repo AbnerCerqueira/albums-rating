21eb430f728af81b885ae0f0ac62b1ae964f4297
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2026-02-03 14:41:22 -0300

    :sparkles: feat(mongoose): add utility function to build pipeline to string search

--- src/contexts/!common/mongoose-utils.ts
+++ src/contexts/!common/mongoose-utils.ts
@@ -1,15 +1,61 @@
-import type { Aggregate, Query } from 'mongoose'
+import { escapeRegExp } from 'lodash'
+import type { Aggregate, PipelineStage, Query } from 'mongoose'
 import type { Pagination } from './pagination'
+import { defaultSearchStringOptions, type SearchStringOptions } from './search-options'
+
+function buildSearchStringPipeline(
+  fields: Record<string, string>,
+  options: SearchStringOptions = defaultSearchStringOptions
+): PipelineStage[] {
+  const validEntries = Object.entries(fields).filter(([, value]) =>
+    value?.trim()
+  )
+
+  if (!validEntries.length) {
+    return []
+  }
+
+  const { matchType, combineWith } = options
+
+  const matchStage: PipelineStage.Match = {
+    $match: {},
+  }
+
+  const buildValueByMatchType = (value: string) =>
+    matchType === 'perfect'
+      ? value
+      : { $regex: `^${escapeRegExp(value)}`, $options: 'i' }
+
+  if (combineWith === 'or') {
+    matchStage.$match = { $or: [] }
+
+    for (const [key, value] of validEntries) {
+      matchStage.$match.$or?.push({ [key]: buildValueByMatchType(value) })
+    }
+
+    return [matchStage]
+  }
+
+  for (const [key, value] of validEntries) {
+    matchStage.$match[key] = buildValueByMatchType(value)
+  }
+
+  return [matchStage]
+}
 
 function withPagination(
   // biome-ignore lint/suspicious/noExplicitAny: necessário
   aggregate: Aggregate<any> | Query<any, any>,
-  pagination: Pagination
+  pagination?: Pagination
 ): void {
+  if (!pagination) {
+    return
+  }
   const { page, size } = pagination
   aggregate.skip(size * (page - 1)).limit(size)
 }
 
 export const MongooseUtils = {
   withPagination,
+  buildSearchStringPipeline,
 }
---
+++ src/contexts/!common/search-options.ts
@@ -0,0 +1,15 @@
+export const MATCH_TYPES = ['perfect', 'startsWith'] as const
+export const COMBINE_WITH = ['and', 'or'] as const
+
+export type MatchType = (typeof MATCH_TYPES)[number]
+export type CombineWith = (typeof COMBINE_WITH)[number]
+
+export type SearchStringOptions = {
+  matchType: MatchType
+  combineWith: CombineWith
+}
+
+export const defaultSearchStringOptions: SearchStringOptions = {
+  matchType: 'perfect',
+  combineWith: 'and',
+}
